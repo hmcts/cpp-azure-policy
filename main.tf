fafa6c594b19c326e2daf09c914abb1d4d0437e9
Simran Sian <simran.sian1@justice.gov.uk>
2025-09-15 14:49:25 +0100

    Re-add management assignments

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "azurerm_subscription_policy_assignment" "subscription_assignments" {
   depends_on = [
     azurerm_policy_definition.policies
   ]
-}
+}*/
 
 resource "azurerm_management_group_policy_assignment" "management_assignments" {
   for_each = local.management_assignments
@@ -74,4 +74,4 @@ resource "azurerm_management_group_policy_assignment" "management_assignments" {
 
   # Need policy assignments to be defined before we can reference them
   depends_on = [azurerm_policy_definition.policies]
-}*/
+}
