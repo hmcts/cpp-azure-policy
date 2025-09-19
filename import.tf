f0057516c1e84aabdd39c7e39647e950582db3c5
Simran Sian <simran.sian1@justice.gov.uk>
2025-09-19 14:54:45 +0100

    Add policy definitions

--- import.tf
+++ import.tf
@@ -1,19 +1,5 @@
-/*import {
-  for_each = var.env == "live" ? [1] : []
-  id = "/subscriptions/9ab65d81-930d-4cc0-a93d-367e14676bc0/providers/Microsoft.Authorization/policyDefinitions/Deny Resource Deletion"
-  to = azurerm_policy_definition.policies["CppDenyResourceDeletion"]
-}*/
-
-resource "azurerm_subscription_policy_assignment" "existing" {
-  name                 = "Deny-Resource-Deletion-Assignment"
-  policy_definition_id = "/subscriptions/9ab65d81-930d-4cc0-a93d-367e14676bc0/providers/Microsoft.Authorization/policyDefinitions/Deny Resource Deletion"
-  subscription_id      = "/subscriptions/9ab65d81-930d-4cc0-a93d-367e14676bc0"
-
-}
-
 import {
   for_each = var.env == "live" ? [1] : []
-  id       = "/subscriptions/9ab65d81-930d-4cc0-a93d-367e14676bc0/providers/Microsoft.Authorization/policyAssignments/Deny-Resource-Deletion-Assignment"
-  to       = azurerm_subscription_policy_assignment.existing
-
+  id       = "/subscriptions/9ab65d81-930d-4cc0-a93d-367e14676bc0/providers/Microsoft.Authorization/policyDefinitions/Deny Resource Deletion"
+  to       = azurerm_policy_definition.policies["CppDenyResourceDeletion"]
 }
